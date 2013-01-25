53bec702b7dcbec3de1c4903aa89c50a4d8bad41
Eipifi <eipifi@gmail.com>
2013-01-25 23:58:33 +0100

    Thread-based server works

--- src/ariadne/Ariadne.java
+++ src/ariadne/Ariadne.java
@@ -1,59 +1,24 @@
 package ariadne;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import ariadne.net.Conversation;
-import ariadne.net.Dispatcher;
-import ariadne.net.SocketChannelDispatcher;
+import ariadne.net.Server;
 import ariadne.ui.UI;
 import ariadne.ui.DelegableUI.HashAddedEvent;
 import ariadne.ui.UI.Event;
 import ariadne.ui.UI.Listener;
 import ariadne.ui.graphic.GraphicUI;
 
-
 public class Ariadne {
 	public static void main(String[] args){
 		
-		Conversation c;
-		Dispatcher d = new SocketChannelDispatcher(25566);
+		final Server s = new Server(25566);
+		s.start(4);
 		
-		d.enable();
-		c = d.accept();
-		ByteBuffer b = ByteBuffer.allocate(1024);
-		try {
-			c.getSocket().read(b);
-			for(int i=0;i<b.position();++i){
-				System.out.println((char) b.get(i));
-			}
-			
-			System.out.println(b);
-		} catch (IOException e) {
-			System.out.println("IOExc");
-		}
-		d.await(c);
-		c = d.accept();
-		try {
-			c.getSocket().read(b);
-			for(int i=0;i<b.position();++i){
-				System.out.println((char) b.get(i));
-			}
-			
-			System.out.println(b);
-		} catch (IOException e) {
-			System.out.println("IOExc");
-		}
-		
-		d.dispose(c);
-		d.disable();
-		
-		/*
 		final UI ui = new GraphicUI();
 		ui.open();
 		ui.onClosing(new Listener(){
 			public void trigger(Event e) {
 				ui.close();
+				s.stop();
 			}});
 		
 		ui.onHashAdded(new Listener(){
@@ -64,6 +29,6 @@ public class Ariadne {
 		
 		ui.eventLoop();
 		ui.close();
-		*/
+	
 	}
 }
--- src/ariadne/net/Server.java
+++ src/ariadne/net/Server.java
@@ -1,18 +1,40 @@
 package ariadne.net;
 
+import java.util.ArrayList;
+
 public class Server {
 	private Dispatcher dispatcher;
+	private ArrayList<Worker> workers;
+	private boolean running;
 	
-	public Server(){
-		throw new UnsupportedOperationException("Not yet implemented");
-		//construct the dispatcher
+	public Server(int port){
+		dispatcher = new SocketChannelDispatcher(port);
+		workers = new ArrayList<Worker>();
+		running = false;
 	}
-	
 	
 	public void start(int threadCount){
+		stop();
 		
+		dispatcher.enable();
+		for(int i=0;i<threadCount;++i){
+			Worker w = new WorkHandler();
+			w.setDispatcher(dispatcher);
+			Thread t = new Thread(w);
+			t.start();
+			workers.add(w);
+		}
+		running = true;
 	}
+	
 	public void stop(){
-		
+		if(running){
+			dispatcher.disable();
+			for(int i=0;i<workers.size();++i){
+				workers.get(i).stopWorking();
+			}
+			workers = new ArrayList<Worker>();
+			running = false;
+		}
 	}
 }
