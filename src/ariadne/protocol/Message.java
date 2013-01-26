f9c9276d036f88e5795779d928ab7c3f4cccf6b7
Eipifi <eipifi@gmail.com>
2013-01-26 03:03:41 +0100

    Just in case, commit.

--- src/ariadne/protocol/Message.java
+++ src/ariadne/protocol/Message.java
@@ -6,6 +6,13 @@ public abstract class Message {
 	private ByteBuffer buf;
 	
 	public void addBytes(ByteBuffer b){
+		if(buf == null){
+			buf = ByteBuffer.allocate(64);
+		}
+		
+		if(b.limit() > buf.remaining()){
+			//ByteBuffer nbuf = ByteBuffer.allocate();
+		}
 		
 	}
 }
