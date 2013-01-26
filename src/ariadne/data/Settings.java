4dbcd05052ae1a762e61b92d0b8680c38ec59da0
Luna <root@bt.foo.org>
2013-01-26 19:53:14 +0100

    Settings (working i hope), base for Responses

---
+++ src/ariadne/data/Settings.java
@@ -0,0 +1,76 @@
+package ariadne.data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import ariadne.data.Hash.InvalidHashException;
+import ariadne.utils.Log;
+
+/*
+ * File Structure : Hash Name Filepath
+ */
+public class Settings {
+	private static Hash[] hashes;
+	private static String[] names;
+	private static String[] filePath;
+
+	public Settings() {
+		BufferedReader br = null;
+		try {
+			String sCurrentLine;
+			String temp = "";
+			int i = 0, j = 0;
+			br = new BufferedReader(new FileReader(".ariadneSettings"));
+			while ((sCurrentLine = br.readLine()) != null) {
+				while (sCurrentLine != " ") {
+					temp += sCurrentLine.charAt(i);
+					i++;
+				}
+				try {
+					hashes[j] = new Hash(temp);
+				} catch (InvalidHashException e) {
+					Log.error("File Hash incorrect.");
+				}
+				temp = "";
+				while (sCurrentLine != " ") {
+					temp += sCurrentLine.charAt(i);
+					i++;
+				}
+				names[j] = temp;
+				temp = "";
+				while (sCurrentLine != "\n") {
+					temp += sCurrentLine.charAt(i);
+					i++;
+				}
+				filePath[j] = temp;
+				temp = "";
+				j++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+
+		}
+	}
+
+	public Hash[] getAllHash() {
+		return hashes;
+	}
+
+	public String getNameForHash(Hash hash) {
+		int j = 0;
+		for (Hash hashTemp : hashes) {
+			if (hashTemp == hash)
+				return names[j];
+			j++;
+		}
+		return null;
+	}
+}
---
+++ src/ariadne/protocol/ResponsePeers.java
@@ -0,0 +1,17 @@
+package ariadne.protocol;
+
+public class ResponsePeers extends Response{
+
+	@Override
+	public byte[] getContent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isComplete() throws InvalidMessageException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
