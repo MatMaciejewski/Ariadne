b9f77a578c8e811b0c18d7426664cfb745650cbd
Eipifi <eipifi@gmail.com>
2013-01-26 13:02:48 +0100

    UI work starts...

--- src/ariadne/Application.java
+++ src/ariadne/Application.java
@@ -1,28 +1,27 @@
 package ariadne;
 
-import java.nio.ByteBuffer;
+import ariadne.ui.UI;
+import ariadne.ui.UI.Event;
+import ariadne.ui.UI.Listener;
+import ariadne.ui.graphic.GraphicUI;
 
-import ariadne.data.BitMask;
 
 public class Application{
 	
-	
-	
 	public static void main(String[] args){
+		final UI ui = new GraphicUI();
 		
-		BitMask b = new BitMask(18);
+		ui.onClosing(new Listener(){
+			@Override
+			public void trigger(Event e) {
+				ui.breakEventLoop();
+			}});
 		
-		System.out.println(b.get(5));
-		b.set(5);
-		b.set(8);
-		System.out.println(b.get(5));
-		
-		
-		ByteBuffer f = b.getByteBuffer();
-		f.rewind();
-		while(f.position() < f.limit()){
-			System.out.println(f.get());
-		}
+		ui.onHashAdded(new Listener(){
+			@Override
+			public void trigger(Event e) {
+				
+			}});
 		
 	}
 }
