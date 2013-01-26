32e87dc47f3fd5de953f18f55f4093d926088669
Eipifi <eipifi@gmail.com>
2013-01-26 11:53:14 +0100

    Added the QueryDescr class, work in progress...

---
+++ src/ariadne/protocol/QueryDescr.java
@@ -0,0 +1,17 @@
+package ariadne.protocol;
+
+public class QueryDescr extends Query {
+
+	@Override
+	public byte getCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isComplete() throws InvalidMessageException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
