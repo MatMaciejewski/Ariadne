d9776287e0ada34cf332dc30387592f8902921f3
Eipifi <eipifi@gmail.com>
2013-01-29 00:16:19 +0100

    Fixed file reallocation

--- src/ariadne/data/Database.java
+++ src/ariadne/data/Database.java
@@ -3,6 +3,8 @@ package ariadne.data;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import ariadne.utils.Log;
+
 public class Database {
 	private static Map<Hash, File> files;;
 
@@ -14,8 +16,14 @@ public class Database {
 		return files.get(hash);
 	}
 
-	public static void insertFile(Descriptor desc, BitMask bit, String path, String name) {
-		files.put(desc.getHash(), new File(desc, bit, path, name));
+	public static void insertFile(Descriptor desc, BitMask bit, String path, String name, boolean reallocate) {
+		File f = new File(desc, bit, path, name);
+		if(reallocate){
+			if(!f.reallocate()){
+				Log.error("Could not reallocate file " + name);
+			}
+		}
+		files.put(desc.getHash(), f);
 	}
 
 	public static void removeFile(Hash hash, boolean removeFromDisk) {
